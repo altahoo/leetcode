90d2f553d6ad4626a36e4c4a0d7c40047f065ad8
altahoo <tinglihu2013@gmail.com>
2023-08-05 09:05:17 -0400

    Create 238_Product of Array Except Self.py

---
+++ array/238_Product of Array Except Self.py
@@ -0,0 +1,22 @@
+# 238. Product of Array Except Self
+
+# Example 1:
+
+# Input: nums = [1,2,3,4]
+# Output: [24,12,8,6]
+
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        left, right = [1] * n, [1] * n
+        for i in range(1, n):
+            left[i] = nums[i - 1] * left[i - 1]
+        
+        for i in range(n - 2, -1, -1):
+            right[i] = nums[i + 1] * right[i + 1]
+
+        result = []
+        for i in range(n):
+            result.append(left[i] * right[i])
+        
+        return result
