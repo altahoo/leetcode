10bc53ec39322b3e147dc699d20122b9a4cf6a75
altahoo <tinglihu2013@gmail.com>
2023-09-13 19:43:59 -0400

    Update 532_K-diff Pairs in an Array.py

--- hash_table/532_K-diff Pairs in an Array.py
+++ hash_table/532_K-diff Pairs in an Array.py
@@ -13,3 +13,15 @@ class Solution:
             if k != 0 and counter[num - k] > 0:
                 result.add((num - k, num))
         return len(result)
+
+
+class Solution:
+    def findPairs(self, nums: List[int], k: int) -> int:
+        counter = collections.Counter(nums)
+        result = 0
+        for num, count in counter.items():
+            if k == 0 and count > 1:
+                result += 1
+            elif k > 0 and counter[num + k] > 0:
+                result += 1
+        return result
