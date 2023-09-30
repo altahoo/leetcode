6e9614565dd5454b05375bf1d5117f8053c73687
altahoo <tinglihu2013@gmail.com>
2023-09-29 21:04:16 -0400

    Update 3_Longest Substring Without Repeating Characters.py

--- all/3_Longest Substring Without Repeating Characters.py
+++ all/3_Longest Substring Without Repeating Characters.py
@@ -17,3 +17,18 @@ class Solution:
             left += 1
             visited = set(s[left: i + 1])
         return max_length
+
+
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        left = 0
+        char_to_idx = {}
+        max_length = 0
+        for i, char in enumerate(s):
+            if char not in char_to_idx or char_to_idx[char] < left:
+                max_length = max(max_length, i - left + 1)
+                char_to_idx[char] = i
+                continue
+            left = char_to_idx[char] + 1
+            char_to_idx[char] = i
+        return max_length
