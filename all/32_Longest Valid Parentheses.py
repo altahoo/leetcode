8988b0869151d41aeefa473575e711c103a2f988
altahoo <tinglihu2013@gmail.com>
2023-10-09 18:21:58 -0400

    Update 32_Longest Valid Parentheses.py

--- all/32_Longest Valid Parentheses.py
+++ all/32_Longest Valid Parentheses.py
@@ -3,16 +3,27 @@
 class Solution:
     def longestValidParentheses(self, s: str) -> int:
         result = 0
-        start = 0
-        stack = []
+        left, right = 0, 0
+        n = len(s)
 
-        for i in range(len(s)):
+        for i in range(n):
             if s[i] == '(':
-                stack.append(i)
-                continue
-            if not stack:
-                start = i + 1
-                continue
-            stack.pop()
-            result = max(result, i - start + 1) if not stack else max(result, i - stack[0])
+                left += 1
+            else:
+                right += 1
+            if left == right:
+                result = max(result, 2 * left)
+            elif left < right:
+                left, right = 0, 0
+        
+        left, right = 0, 0
+        for i in range(n - 1, -1, -1):
+            if s[i] == '(':
+                left += 1
+            else:
+                right += 1
+            if left == right:
+                result = max(result, 2 * left)
+            elif left > right:
+                left, right = 0, 0
         return result
