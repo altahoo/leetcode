ba829b350b4535464525df418e0859a5480378fe
altahoo <tinglihu2013@gmail.com>
2023-10-14 12:47:13 -0400

    Update 46_Permutations.py

--- all/46_Permutations.py
+++ all/46_Permutations.py
@@ -1,1 +1,18 @@
-46. Permutations
+# 46. Permutations
+
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        result = []
+        n = len(nums)
+
+        def _permute(start):
+            if start == n - 1:
+                result.append(nums.copy())
+                return
+            for i in range(start, n):
+                nums[start], nums[i] = nums[i], nums[start]
+                _permute(start + 1)
+                nums[start], nums[i] = nums[i], nums[start]
+
+        _permute(0)
+        return result
