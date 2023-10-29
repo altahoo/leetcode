9dc7820d19a1ac1cafea30f22e9ec478a39fe11f
altahoo <tinglihu2013@gmail.com>
2023-10-28 23:03:05 -0400

    Create 89_Gray Code.py

---
+++ all/89_Gray Code.py
@@ -0,0 +1,18 @@
+# 89. Gray Code
+
+# An n-bit gray code sequence is a sequence of 2n integers where:
+
+# Every integer is in the inclusive range [0, 2n - 1],
+# The first integer is 0,
+# An integer appears no more than once in the sequence,
+# The binary representation of every pair of adjacent integers differs by exactly one bit, and
+# The binary representation of the first and last integers differs by exactly one bit.
+# Given an integer n, return any valid n-bit gray code sequence.
+
+class Solution:
+    def grayCode(self, n: int) -> List[int]:
+        result = []
+        for i in range(2**n):
+            result.append((i>>1)^i)
+        return result
+        
