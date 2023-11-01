0b423b800e777221738185367c8bce297e42f431
altahoo <tinglihu2013@gmail.com>
2023-11-01 18:20:44 -0400

    Create 101_Symmetric Tree.py

---
+++ all/101_Symmetric Tree.py
@@ -0,0 +1,27 @@
+# 101. Symmetric Tree
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        if not root:
+            return True
+
+        queue = collections.deque([root])
+        while queue:
+            level = []
+            for _ in range(len(queue)):
+                node = queue.popleft()
+                if node:
+                    level.append(node.val)
+                    queue.append(node.left)
+                    queue.append(node.right)
+                else:
+                    level.append(None)
+            if level != level[::-1]:
+                return False
+        return True
