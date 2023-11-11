fca4793ebdd6a65486a089114cbadb8affb35ab3
altahoo <tinglihu2013@gmail.com>
2023-11-11 17:18:21 -0500

    Create 116_Populating Next Right Pointers in Each Node.py

---
+++ all/116_Populating Next Right Pointers in Each Node.py
@@ -0,0 +1,43 @@
+# 116. Populating Next Right Pointers in Each Node
+
+# You are given a perfect binary tree where all leaves are on the same level, and every parent has two children. The binary tree has the following definition:
+
+# struct Node {
+#   int val;
+#   Node *left;
+#   Node *right;
+#   Node *next;
+# }
+# Populate each next pointer to point to its next right node. If there is no next right node, the next pointer should be set to NULL.
+
+# Initially, all next pointers are set to NULL.
+
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+
+class Solution:
+    def connect(self, root: 'Optional[Node]') -> 'Optional[Node]':
+        if not root:
+            return root
+        
+        queue = collections.deque()
+        queue.append(root)
+
+        while queue:
+            for i in range(len(queue) - 1):
+                queue[i].next = queue[i + 1]
+            
+            for _ in range(len(queue)):
+                node = queue.popleft()
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+        return root
