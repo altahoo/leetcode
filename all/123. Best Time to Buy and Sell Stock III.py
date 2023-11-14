7834e48d1ac5e7832cc41d71917bf4a07cceb8e0
altahoo <tinglihu2013@gmail.com>
2023-11-13 20:00:17 -0500

    Update 123. Best Time to Buy and Sell Stock III.py

--- all/123. Best Time to Buy and Sell Stock III.py
+++ all/123. Best Time to Buy and Sell Stock III.py
@@ -5,3 +5,19 @@
 # Find the maximum profit you can achieve. You may complete at most two transactions.
 
 # Note: You may not engage in multiple transactions simultaneously (i.e., you must sell the stock before you buy again).
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        # four states
+        # buy 1st stock
+        # sell 1st stock
+        # buy 2nd stock
+        # sell 2nd stock
+        s1, s2, s3, s4 = -prices[0], float('-inf'), float('-inf'), float('-inf')
+        for price in prices:
+            s1 = max(s1, -price)
+            s2 = max(s2, s1 + price)
+            s3 = max(s3, s2 - price)
+            s4 = max(s4, s3 + price)
+        
+        return max(s4, 0)
