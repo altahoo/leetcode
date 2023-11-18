43c47c1dfeb0289db702481a098c4ec0e345ba18
altahoo <tinglihu2013@gmail.com>
2023-11-18 11:54:31 -0500

    Create 137_Single Number II.py

---
+++ all/137_Single Number II.py
@@ -0,0 +1,13 @@
+# 137. Single Number II
+
+# Given an integer array nums where every element appears three times except for one, which appears exactly once. Find the single element and return it.
+
+# You must implement a solution with a linear runtime complexity and use only constant extra space.
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        ones, twos = 0, 0  # The bits that have appeared once and twice
+        for num in nums:
+            ones ^= (num & ~twos)
+            twos ^= (num & ~ones)
+        return ones
