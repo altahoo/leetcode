b14ac12e733fa62ddbb31d903d6a8da316da7db3
altahoo <tinglihu2013@gmail.com>
2023-11-22 12:47:03 -0500

    Create 147_Insertion Sort List.py

---
+++ all/147_Insertion Sort List.py
@@ -0,0 +1,26 @@
+# 147. Insertion Sort List
+
+# The steps of the insertion sort algorithm:
+
+# Insertion sort iterates, consuming one input element each repetition and growing a sorted output list.
+# At each iteration, insertion sort removes one element from the input data, finds the location it belongs within the sorted list and inserts it there.
+# It repeats until no input elements remain.
+# The following is a graphical example of the insertion sort algorithm. The partially sorted list (black) initially contains only the first element in the list. One element (red) is removed from the input data and inserted in-place into the sorted list with each iteration.
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def insertionSortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode(0)
+        while head:
+            tmp = head.next
+            cur = dummy
+            while cur.next and cur.next.val <= head.val:
+                cur = cur.next
+            head.next = cur.next
+            cur.next = head
+            head = tmp
+        return dummy.next
