0b1496b12d673b2a5e699faed8571a90440c45ed
altahoo <tinglihu2013@gmail.com>
2023-12-10 22:00:05 -0500

    Create 216_Combination Sum III.py

---
+++ all/216_Combination Sum III.py
@@ -0,0 +1,24 @@
+# 216. Combination Sum III
+
+# Find all valid combinations of k numbers that sum up to n such that the following conditions are true:
+
+# Only numbers 1 through 9 are used.
+# Each number is used at most once.
+# Return a list of all possible valid combinations. The list must not contain the same combination twice, and the combinations may be returned in any order.
+
+class Solution:
+    def combinationSum3(self, k: int, n: int) -> List[List[int]]:
+        result = []
+
+        def dfs(k, n, sol):
+            if n < 0 or k < 0:
+                return
+            if n == 0 and k == 0:
+                result.append(sol)
+            
+            start = sol[-1] + 1 if sol else 1
+            for i in range(start, 10):
+                dfs(k - 1, n - i, sol + [i])
+        
+        dfs(k, n, [])
+        return result  
