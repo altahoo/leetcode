c825d6b2815892677fdffe40af9c347017cbaef0
altahoo <tinglihu2013@gmail.com>
2024-01-12 13:20:50 -0500

    Create 293_Flip Game.py

---
+++ all/293_Flip Game.py
@@ -0,0 +1,15 @@
+# 293. Flip Game
+
+# You are playing a Flip Game with your friend.
+
+# You are given a string currentState that contains only '+' and '-'. You and your friend take turns to flip two consecutive "++" into "--". The game ends when a person can no longer make a move, and therefore the other person will be the winner.
+
+# Return all possible states of the string currentState after one valid move. You may return the answer in any order. If there is no valid move, return an empty list [].
+
+class Solution:
+    def generatePossibleNextMoves(self, currentState: str) -> List[str]:
+        result = []
+        for i in range(len(currentState) - 1):
+            if currentState[i:i + 2] == '++':
+                result.append(currentState[:i] + '--' + currentState[i+2:])
+        return result
