542665b6ec7340d301f8d2060ec4524692b889b9
altahoo <tinglihu2013@gmail.com>
2024-01-13 17:54:55 -0500

    Create 297_Serialize and Deserialize Binary Tree.py

---
+++ all/297_Serialize and Deserialize Binary Tree.py
@@ -0,0 +1,61 @@
+# 297. Serialize and Deserialize Binary Tree
+
+# Serialization is the process of converting a data structure or object into a sequence of bits so that it can be stored in a file or memory buffer, or transmitted across a network connection link to be reconstructed later in the same or another computer environment.
+
+# Design an algorithm to serialize and deserialize a binary tree. There is no restriction on how your serialization/deserialization algorithm should work. You just need to ensure that a binary tree can be serialized to a string and this string can be deserialized to the original tree structure.
+
+# Clarification: The input/output format is the same as how LeetCode serializes a binary tree. You do not necessarily need to follow this format, so please be creative and come up with different approaches yourself.
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Codec:
+
+    def serialize(self, root):
+        """Encodes a tree to a single string.
+        
+        :type root: TreeNode
+        :rtype: str
+        """
+        def s_helper(node, string):
+            if node is None:
+                string += 'None,'
+            else:
+                string += str(node.val) + ','
+                string = s_helper(node.left, string)
+                string = s_helper(node.right, string)
+            return string
+
+        return s_helper(root, '')
+        
+
+    def deserialize(self, data):
+        """Decodes your encoded data to tree.
+        
+        :type data: str
+        :rtype: TreeNode
+        """
+        def d_helper(l):
+            if l[0] == 'None':
+                l.pop(0)
+                return None
+            
+            root = TreeNode(l[0])
+            l.pop(0)
+            root.left = d_helper(l)
+            root.right = d_helper(l)
+            return root
+            
+        str_list = data.split(',')
+        root = d_helper(str_list)
+        return root
+        
+
+# Your Codec object will be instantiated and called as such:
+# ser = Codec()
+# deser = Codec()
+# ans = deser.deserialize(ser.serialize(root))
