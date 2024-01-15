a3528894df3951dd3af1bf72563a4664cd1c71d4
altahoo <tinglihu2013@gmail.com>
2024-01-15 15:41:51 -0500

    Update 301_Remove Invalid Parentheses.py

--- all/301_Remove Invalid Parentheses.py
+++ all/301_Remove Invalid Parentheses.py
@@ -1,5 +1,29 @@
-301. Remove Invalid Parentheses
+# 301. Remove Invalid Parentheses
 
-Given a string s that contains parentheses and letters, remove the minimum number of invalid parentheses to make the input string valid.
+# Given a string s that contains parentheses and letters, remove the minimum number of invalid parentheses to make the input string valid.
 
-Return a list of unique strings that are valid with the minimum number of removals. You may return the answer in any order.
+# Return a list of unique strings that are valid with the minimum number of removals. You may return the answer in any order.
+
+class Solution:
+    def removeInvalidParentheses(self, s: str) -> List[str]:
+        result = []
+        visited = set([s])
+        queue = collections.deque([s])
+        found = False
+        
+        while queue:
+            t = queue.popleft()
+            if self.isValid(t):
+                result.append(t)
+                found = True
+            if found:
+                continue
+            for i in range(len(t)):
+                if t[i] not in ['(', ')']:
+                    continue
+                string = t[:i] + t[i + 1:]
+                if string not in visited:
+                    queue.append(string)
+                    visited.add(string)
+        
+        return result if result else ['']
