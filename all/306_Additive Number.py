03418cbb94864cab9206d8f4ea2f71f4ed861e00
altahoo <tinglihu2013@gmail.com>
2024-01-17 19:06:04 -0500

    Update 306_Additive Number.py

--- all/306_Additive Number.py
+++ all/306_Additive Number.py
@@ -11,27 +11,32 @@
 
 class Solution:
     def isAdditiveNumber(self, num: str) -> bool:
-        for i in range(1, len(num)):
-            s1 = num[:i]   
-            if len(s1) > 1 and s1[0] == '0':
+        n = len(num)
+        for i in range(1, n):
+            first = num[:i]
+            if len(first) > 1 and first[0] == '0':
                 continue
-            for j in range(i + 1, len(num)):
-                d1 = int(s1)
-                s2 = num[i: j]
-                if len(s2) > 1 and s2[0] == '0':
+
+            for j in range(i + 1, n):
+                num1 = int(first)
+                second = num[i: j]
+                if len(second) > 1 and second[0] == '0':
                     continue
-                d2 = int(s2)
-                next_d = d1 + d2
-                next_str = str(d1 + d2)
-                if next_str != num[j: j + len(next_str)]:
+                num2 = int(second)
+
+                num_sum = num1 + num2
+                str_sum = str(num_sum)
+                if str_sum != num[j:j+len(str_sum)]:
                     continue
-                all_str = s1 + s2 + next_str
-                while len(all_str) < len(num):
-                    d1 = d2
-                    d2 = next_d
-                    next_d = d1 + d2
-                    next_str = str(next_d)
-                    all_str += next_str
-                if all_str == num:
+                
+                solution = first + second + str_sum
+                while len(solution) < n:
+                    num1 = num2
+                    num2 = num_sum
+                    num_sum = num1 + num2
+                    str_sum = str(num_sum)
+                    solution += str_sum
+                
+                if solution == num:
                     return True
         return False
